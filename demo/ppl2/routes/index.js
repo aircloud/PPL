599b3f0711e77900f8973a759337f132a0ea4146
xiaotao Nie <onlythen@yeah.net>
2016-12-21 23:07:44 +0800

    重构了demo的目录

--- demo/ppl2/routes/index.js
+++ demo/ppl2/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var parser = require('../control/UglifyJS/uglify-js').parser;
+var genCoder = require('../control/UglifyJS/uglify-js').uglify;
+var Tree = require('treeify');
+
+var Convert = require("../control/convert.control");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post("/convert",function(req,res,next){
+
+  //这里调用函数，返回代码
+
+});
+
 
 module.exports = router;
